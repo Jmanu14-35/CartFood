12b68c35a3e056a301c21d07f74586230bdcd3c2
jaivalos-dev <jaivalos.dev@gmail.com>
2024-04-19 10:25:10 -0600

    Pagosssssss

--- icard_django/icard/urls.py
+++ icard_django/icard/urls.py
@@ -26,6 +26,7 @@ from categories.api.router import router_category
 from products.api.router import router_product
 from tables.api.router import router_table
 from orders.api.router import router_orders
+from payments.api.router import router_payments
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -50,6 +51,7 @@ urlpatterns = [
     path('api/', include(router_product.urls)),
     path('api/', include(router_table.urls)),
     path('api/', include(router_orders.urls)),
+    path('api/', include(router_payments.urls))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
